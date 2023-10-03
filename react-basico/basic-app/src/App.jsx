f9daf8babda563586a13f6eeec81d99b78947ea8
Ana Bordin <118741376+AnnyaB@users.noreply.github.com>
2023-10-03 12:31:24 -0300

    Aula0310 Componentes e Funcões

--- react-basico/basic-app/src/App.jsx
+++ react-basico/basic-app/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AppViewer from './components/AppViewer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1> Meu primeiro webApplication
+    <h1> 
+      Meu primeiro webApplication
     </h1>
+    <AppViewer />
     </>
   )
   }
--- react-basico/basic-app/src/components/AppViewer.jsx
+++ react-basico/basic-app/src/components/AppViewer.jsx
@@ -1,7 +1,18 @@
 import styles from './AppViewer.module.css'
+//camada de compilação de serviços
+function somaNumero(n1, n2)
+{
+  return(n1+n2)
+}
 const AppViewer = () => {
+  //retorno no display
   return (
-    <div className={styles.AvContainer}>AppViewer</div>
+    <>
+      <div className={styles.AvContainer}>AppViewer</div>
+      <p>
+        {somaNumero(5,17)}
+      </p>
+    </>
   )
 }
 
