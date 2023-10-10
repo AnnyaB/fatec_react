4fa78447ab41b9b637a89c927822f89396a2984b
Ana Bordin <118741376+AnnyaB@users.noreply.github.com>
2023-10-10 12:30:24 -0300

    aula 10.10 DW3

--- react-basico/basic-app/src/App.jsx
+++ react-basico/basic-app/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AppViewer from './components/AppViewer'
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +10,9 @@ function App() {
 <>
  <h1>Meu primeiro WebApp</h1>
  <AppViewer />
+ {
+
+ }
 </>
   )
 }
--- react-basico/basic-app/src/components/AppViewer.jsx
+++ react-basico/basic-app/src/components/AppViewer.jsx
@@ -1,13 +1,70 @@
 import styles from './AppViewer.module.css'
 //Camada de Compilação de Serviços
-function somaNumero(n1, n2){
-  return(n1+n2)
+function somaNumero(n1, n2) {
+  return (n1 + n2)
 }
 const AppViewer = () => {
   //Retorno no display
+  const professores = [
+    { id: 1, nome: "Jose Armando Vargas", idade: 42, formacao: "Engenharia da computação", titulacao: "Latu-Senso" },
+    { id: 2, nome: "Maria Silva", idade: 35, formacao: "Ciência da Informação", titulacao: "Mestrado" },
+    { id: 3, nome: "Carlos Oliveira", idade: 28, formacao: "Sistemas de Informação", titulacao: "Bacharelado" },
+    { id: 4, nome: "Ana Souza", idade: 50, formacao: "Engenharia Elétrica", titulacao: "Doutorado" },
+    { id: 5, nome: "Pedro Santos", idade: 38, formacao: "Ciência da Computação", titulacao: "Especialização" },
+    { id: 6, nome: "Fernanda Costa", idade: 45, formacao: "Engenharia de Software", titulacao: "Mestrado" },
+    { id: 7, nome: "Ricardo Pereira", idade: 32, formacao: "Tecnologia da Informação", titulacao: "Graduação" },
+    { id: 8, nome: "Patricia Martins", idade: 48, formacao: "Matemática", titulacao: "Especialização" },
+    { id: 9, nome: "Gustavo Lima", idade: 36, formacao: "Engenharia Mecatrônica", titulacao: "Doutorado" }
+  ]
+
   return (
     <>
-      <div className={somaNumero(2, 2) >10 ? console.log("Bunda"): console.log("Beiço")}>AppViewer</div>
+      <h2>lista de professores - lista</h2>
+      {
+        professores.map((professor) => {
+          return (
+            <div key={professor.id}>
+              <ul>
+                <li><strong>Nome:</strong>{professor.nome}</li>
+                <li><strong>Idade:</strong>{professor.idade}</li>
+                <li><strong>Formação:</strong>{professor.formacao}</li>
+                <li><strong>Titulação:</strong>{professor.titulacao}</li>
+                <li>-----------</li>
+              </ul>
+            </div>
+          )
+        }) // .map mesma coisa do foreach/for em um vetor. Ja a ((promisse)=> {}) é uma promessa que vai voltar algo daquela ação
+      }
+
+      <h2>lista de professores - tabela</h2>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Idade</th>
+          <th>Formação</th>
+          <th>Titulação</th>
+        </tr>
+        {professores.map((professor) => {
+          return (
+            <tr className={professor.id % 2 == 0 ? styles.linhaCinza : null} key={professor.id}>
+              <td>{professor.id}</td>
+              <td>{professor.nome}</td>
+              <td>{professor.idade}</td>
+              <td>{professor.formacao}</td>
+              <td className={professor.titulacao == "Mestrado" || professor.titulacao == "Doutorado"
+                ? styles.superTitulo : styles.normalTitulo}>{professor.titulacao}</td>
+            </tr>
+          )
+        }) // .map mesma coisa do foreach/for em um vetor. Ja a ((promisse)=> {}) é uma promessa que vai voltar algo daquela ação
+        }</table>
+
+      {/*
+        shift, alt, baixo = copia varios elementos um embaixo do outro 
+        alt, cima = move
+        alt, seleção = cria varios atributos ao msm tempo
+        shift, alt, F = formata
+      */}
     </>
   )
 }
